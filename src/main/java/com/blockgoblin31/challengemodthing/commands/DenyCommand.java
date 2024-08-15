40dba05245576efc5ba257fa80c09e981eb15c52
tuc56 <57573828+tuc56@users.noreply.github.com>
2024-08-15 13:22:42 -0500

    msg

---
+++ src/main/java/com/blockgoblin31/challengemodthing/commands/DenyCommand.java
@@ -0,0 +1,24 @@
+package com.blockgoblin31.challengemodthing.commands;
+
+import com.blockgoblin31.challengemodthing.defs.SeVnb;
+import com.mojang.brigadier.CommandDispatcher;
+import net.minecraft.commands.CommandSourceStack;
+import net.minecraft.commands.Commands;
+import net.minecraft.nbt.ByteTag;
+import net.minecraft.server.level.ServerPlayer;
+
+public class DenyCommand {
+    public DenyCommand() {
+
+    }
+
+    public static void register(CommandDispatcher<CommandSourceStack> dispatcher) {
+        dispatcher.register(Commands.literal("deny").executes((command) -> run(command.getSource())));
+    }
+
+    private static int run(CommandSourceStack stack) {
+        ServerPlayer player = stack.getPlayer();
+        player.getPersistentData().put("bg31.deny", ByteTag.valueOf(true));
+        return 1;
+    }
+}
--- src/main/java/com/blockgoblin31/challengemodthing/events/ModEvents.java
+++ src/main/java/com/blockgoblin31/challengemodthing/events/ModEvents.java
@@ -1,16 +1,16 @@
 package com.blockgoblin31.challengemodthing.events;
 
 import com.blockgoblin31.challengemodthing.ChallengeMod;
-import com.blockgoblin31.challengemodthing.commands.AsuiUd;
+import com.blockgoblin31.challengemodthing.commands.DenyCommand;
 import net.minecraftforge.event.RegisterCommandsEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 
 @Mod.EventBusSubscriber(modid = ChallengeMod.MODID)
-public class AsDDui {
+public class ModEvents {
 
     @SubscribeEvent
-    static void aSDdui(RegisterCommandsEvent asdDUi) {
-        AsuiUd.asUiuD(asdDUi.getDispatcher());
+    static void listen(RegisterCommandsEvent asdDUi) {
+        DenyCommand.register(asdDUi.getDispatcher());
     }
 }
